1c098b4eb0f1627bee1c3edefca5696226496075
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-22 23:25:16 +0900

    バッファテクスチャ表示用のQuadとレンダーテクスチャ表示用のQuadを用意

---
+++ Assets/Scripts/MultiRenderTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiRenderTest : MonoBehaviour
+{
+    /// <summary>
+    /// バッファテクスチャ表示用
+    /// </summary>
+    [SerializeField]
+    private MeshRenderer BufferTextureRenderer = null;
+
+    /// <summary>
+    /// レンダーテクスチャ表示用
+    /// </summary>
+    [SerializeField]
+    private MeshRenderer RenderTextureRenderer = null;
+
+    void Awake()
+    {
+
+    }
+}
