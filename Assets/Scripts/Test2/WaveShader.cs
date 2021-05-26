79c9b7a256243e7de9e7b674b62a04da499d0b3d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-26 11:41:40 +0900

    波紋シェーダ実装準備

---
+++ Assets/Scripts/Test2/WaveShader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test2
+{
+    /// <summary>
+    /// 動いた場所から波紋を出すシェーダ
+    /// </summary>
+    public class WaveShader : MonoBehaviour
+    {
+    }
+}
