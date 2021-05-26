0a2f34995bac8a84bcadf7b4d660c5e8f97e51fd
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-26 14:44:13 +0900

    シーン切り替えを実装

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// シーン切り替え
+/// </summary>
+public class SceneChanger : MonoBehaviour
+{
+    void Awake()
+    {
+        this.UpdateAsObservable()
+            .Where((_) => Input.GetKeyDown(KeyCode.Keypad1))
+            .Subscribe((_) => SceneManager.LoadScene("Test1"))
+            .AddTo(gameObject);
+
+        this.UpdateAsObservable()
+            .Where((_) => Input.GetKeyDown(KeyCode.Keypad2))
+            .Subscribe((_) => SceneManager.LoadScene("Test2"))
+            .AddTo(gameObject);
+    }
+}
