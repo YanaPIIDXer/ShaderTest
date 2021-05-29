4fea2ffc09cbc060df2c0f0d11ac546c8f605fb9
YanaPIIDXer <>
2021-05-29 20:54:04 +0900

    とりあえずWebカメラの映像を描画するだけ

---
+++ Assets/Scripts/Test4/PseudoDOF.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test4
+{
+    /// <summary>
+    /// 擬似的な被写界深度
+    /// </summary>
+    [RequireComponent(typeof(FillQuad))]
+    public class PseudoDOF : MonoBehaviour
+    {
+        /// <summary>
+        /// Webカメラ
+        /// </summary>
+        private WebCamera WebCam = new WebCamera();
+
+        void Awake()
+        {
+            WebCam.Initialize();
+            
+            var Renderer = GetComponent<MeshRenderer>();
+            Renderer.material.mainTexture = WebCam.CamTex;
+        }
+    }
+}
--- Assets/Scripts/WebCamera.cs
+++ Assets/Scripts/WebCamera.cs
@@ -7,39 +7,37 @@ using System;
 /// <summary>
 /// Webカメラ
 /// </summary>
-public class WebCamera : MonoBehaviour
+public class WebCamera
 {
-    /// <summary>
-    /// 描画先
-    /// </summary>
-    private ReactiveProperty<RenderTexture> _RenderTarget = new ReactiveProperty<RenderTexture>();
-
-    /// <summary>
-    /// 描画先
-    /// ※Awakeの実行順序に依存しないようにObservableとして外部に公開する
-    /// </summary>
-    public IObservable<RenderTexture> RenderTarget { get { return _RenderTarget; } }
-
     /// <summary>
     /// Webカメラのテクスチャ
     /// </summary>
-    private WebCamTexture CamTex = null;
+    public WebCamTexture CamTex { get; private set; } = null;
 
-    void Awake()
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public WebCamera()
+    {
+    }
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    public void Initialize()
     {
         WebCamDevice Device = WebCamTexture.devices[0];
         CamTex = new WebCamTexture(Device.name, Screen.width, Screen.height, 60);
-
-        var Target = new RenderTexture(1024, 768, 0);
-     
         CamTex.Play();
-        _RenderTarget.Value = Target;
     }
 
-    void OnDestroy()
+    ~WebCamera()
     {
         // シーン遷移が絡む場合、これが無いとカメラを掴みっぱなしになる
         // ※EditorでPlayModeを抜ける時は何故か問題なくカメラを放す
-        CamTex.Stop();
+        if (CamTex != null)
+        {
+            CamTex.Stop();
+        }
     }
 }
