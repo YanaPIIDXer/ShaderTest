b5768a94bb2a1c3c2cd379ff9c1f685b0fbc223d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-29 22:49:35 +0900

    バッファに書き込むフレーム数を調整

--- Assets/Scripts/Test4/PseudoDOF.cs
+++ Assets/Scripts/Test4/PseudoDOF.cs
@@ -32,7 +32,7 @@ namespace Test4
             Mat.SetFloat("_TexelX", 1.0f / 1024);
             Mat.SetFloat("_TexelY", 1.0f / 768);
 
-            Observable.IntervalFrame(10)
+            Observable.IntervalFrame(40)
                       .Subscribe((_) => Graphics.Blit(WebCam.CamTex, BufferTex))
                       .AddTo(gameObject);
         }
